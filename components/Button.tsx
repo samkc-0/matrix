279ecd34214e9a2a9c49026cf10d5f4cf67f575e
samkc-0 <sam.k.c.0@icloud.com>
2025-11-06 13:21:39 +0000

    add nullable onPress callback prop to buttons

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,6 +4,7 @@ import FontAwesome from "@expo/vector-icons/FontAwesome";
 type Props = {
   label: string;
   theme?: "primary";
+  onPress?: () => void;
 };
 
 export default function Button({ label, theme }: Props) {
