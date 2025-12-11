280a398f898018797b589f9bddb2f46ea4bc2ae5
samkc-0 <sam.k.c.0@icloud.com>
2025-12-11 15:55:13 -0600

    fix quadrant rendering

--- components/quadrants.tsx
+++ components/quadrants.tsx
@@ -29,6 +29,8 @@ export default function Quadrants({ children }: Props) {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    width: "100%",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
