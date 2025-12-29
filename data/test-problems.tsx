8f3f64351da545b348bd930648c7224a769163c4
samkc-0 <sam.k.c.0@icloud.com>
2025-12-29 14:18:24 -0600

    add more test problems

--- data/test-problems.tsx
+++ data/test-problems.tsx
@@ -9,6 +9,98 @@ export const testProblems: Problem[] = [
       b: "",
       c: "",
     },
+    a: [[3]],
+    b: [[4]],
+    c: [[12]],
+    omissions: [
+      {
+        matrix: "c",
+        row: 0,
+        col: 0,
+      },
+    ],
+    valid: true,
+    detail: "scalar-scalar",
+  },
+  {
+    type: "matmul",
+    showEquation: false,
+    matrixLabels: {
+      a: "k",
+      b: "v",
+      c: "kv",
+    },
+    a: [[-2]],
+    b: [[5, -1]],
+    c: [[-10, 2]],
+    omissions: [
+      {
+        matrix: "c",
+        row: 0,
+        col: 0,
+      },
+      {
+        matrix: "c",
+        row: 0,
+        col: 1,
+      },
+    ],
+    valid: true,
+    detail: "scalar-vector",
+  },
+  {
+    type: "matmul",
+    showEquation: false,
+    matrixLabels: {
+      a: "",
+      b: "",
+      c: "",
+    },
+    a: [
+      [1, 2],
+      [3, 0],
+    ],
+    b: [
+      [4, 1],
+      [2, 3],
+    ],
+    c: [
+      [8, 7],
+      [12, 3],
+    ],
+    omissions: [
+      {
+        matrix: "c",
+        row: 0,
+        col: 0,
+      },
+      {
+        matrix: "c",
+        row: 0,
+        col: 1,
+      },
+      {
+        matrix: "c",
+        row: 1,
+        col: 0,
+      },
+      {
+        matrix: "c",
+        row: 1,
+        col: 1,
+      },
+    ],
+    valid: true,
+    detail: "2x2",
+  },
+  {
+    type: "matmul",
+    showEquation: false,
+    matrixLabels: {
+      a: "",
+      b: "",
+      c: "",
+    },
     a: [
       [2, 7, 9],
       [0, 0, 3],
